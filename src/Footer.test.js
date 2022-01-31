7074ea5ccaccf28c78f55dae29eba98b0189f5aa
kimberlymo <kimberly.moorhouse@lernende.bbw.ch>
2022-01-31 09:11:09 +0100

    design has been updated and a little bit of code clean-up.

--- src/Footer.test.js
+++ src/Footer.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import Footer from './Footer';
+import Footer from './components/Footer';
 
 test('footer exists', () => {
   render(<Footer />);
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,14 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
+      <nav className="navbar navbar-expand-lg" style={{backgroundColor: '#f8ddac'}}>
+        <div className="container-fluid">
             <img
               width="150px"
               alt="Carved Rock Fitness"
               src={process.env.PUBLIC_URL + '/images/logo.png'}
             />
-          </li>
-        </ul>
+        </div>
       </nav>
-    </header>
   );
 }
