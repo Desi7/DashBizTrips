10a966946038ca01eb1a3305f598fcbc53d38d26
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-01-31 19:25:41 +0100

    fixed formatting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 export default function Header() {
   return (
-      <nav className="navbar navbar-expand-lg" style={{backgroundColor: '#f8ddac'}}>
-        <div className="container-fluid">
-            <img
-              width="150px"
-              alt="Carved Rock Fitness"
-              src={process.env.PUBLIC_URL + '/images/logo.png'}
-            />
-        </div>
-      </nav>
+    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#f8ddac' }}>
+      <div className="container-fluid">
+        <img
+          width="150px"
+          alt="Carved Rock Fitness"
+          src={process.env.PUBLIC_URL + '/images/logo.png'}
+        />
+      </div>
+    </nav>
   );
 }
